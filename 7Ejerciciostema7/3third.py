a544a43625eb6d29764c79b49b3e6e65c8a8a546
DensyVilomar <densy.vilomar09@gmail.com>
2023-05-21 22:01:00 -0400

    Something

--- 7Ejerciciostema7/3third.py
+++ 7Ejerciciostema7/3third.py
@@ -1,3 +1,4 @@
+counter = 0
 Request = input("Insert a name of a file: ")
 try:
     file = open(Request, "r")
@@ -7,10 +8,13 @@ except:
     
 for lines in file:
     if lines.startswith("X-DSPAM-Confidence"):
+        counter += 1
         lines = lines.rstrip()
         search = lines.find(":")
-        decimal = lines[search + 1:]
-        to_float = float(decimal)
-        print(decimal)
+        decimal = float(lines[search + 1:])
+        sum = decimal + decimal
 
+        
 file.close()
+
+print(sum/counter)
