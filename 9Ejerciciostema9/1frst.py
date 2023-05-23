4e4f70981e001987a5af6c3f8ef66d534c7e7de3
DensyVilomar <densy.vilomar09@gmail.com>
2023-05-23 09:31:36 -0400

    A

---
+++ 9Ejerciciostema9/1frst.py
@@ -0,0 +1,11 @@
+my_dict = {}
+with open("zz.txt", "r") as file:
+    for lines in file:
+        if lines.startswith("From "):
+            lines = lines.split()
+            word = lines[2]
+            if word not in my_dict:
+                my_dict[word] = 1
+            else:
+                my_dict[word] += 1
+print(my_dict)
